3be6bb97b99b63d75ca163c65d5f9d70517cc34d
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-13 17:15:01 -0500

    created Xcode project

---
+++ Sources/BSON/BSON.swift
@@ -0,0 +1,25 @@
+//
+//  BSON.swift
+//  BSON
+//
+//  Created by Alsey Coleman Miller on 12/13/15.
+//  Copyright © 2015 PureSwift. All rights reserved.
+//
+
+import SwiftFoundation
+import BSON
+
+/// [Binary JSON](http://bsonspec.org)
+///
+///
+public struct BSON {
+    
+    public typealias Array = [BSON.Value]
+    
+    public typealias Document = [String: BSON.Value]
+    
+    public enum Value {
+        
+        
+    }
+}
---
+++ Xcode/BSON/BSON/BSON.h
@@ -0,0 +1,19 @@
+//
+//  BSON.h
+//  BSON
+//
+//  Created by Alsey Coleman Miller on 12/13/15.
+//  Copyright © 2015 PureSwift. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BSON.
+FOUNDATION_EXPORT double BSONVersionNumber;
+
+//! Project version string for BSON.
+FOUNDATION_EXPORT const unsigned char BSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BSON/PublicHeader.h>
+
+
