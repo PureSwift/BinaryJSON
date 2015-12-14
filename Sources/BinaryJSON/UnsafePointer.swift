093a8d9c85fdf8efeb26df9c44f5ca662183d0c9
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-13 23:33:46 -0500

    working on BSON.appendValue()

--- Sources/BinaryJSON/UnsafePointer.swift
+++ Sources/BinaryJSON/UnsafePointer.swift
@@ -23,43 +23,74 @@ public extension BSON {
         
         for (key, value) in document {
             
-            let keyLength = Int32(key.utf8.count)
-            
-            switch value {
-                
-            case .Null:
-                
-                guard bson_append_null(documentPointer, key, keyLength) == true
-                    else { return nil }
-                
-            case let .String(string):
-                
-                let stringLength = Int32(string.utf8.count)
-                
-                guard bson_append_utf8(documentPointer, key, keyLength, string, stringLength) == true
-                    else { return nil }
-                
-            case let .Date(date):
-                
-                let secondsSince1970 = Int64(date.timeIntervalSince1970)
-                
-                guard bson_append_date_time(documentPointer, key, keyLength, secondsSince1970) == true
-                    else { return nil }
-                
-            case let .Data(data):
-                
-                break
-                
-            case let .Document(document):
-                
-                guard bson_append_document(<#T##bson: UnsafeMutablePointer<bson_t>##UnsafeMutablePointer<bson_t>#>, <#T##key: UnsafePointer<Int8>##UnsafePointer<Int8>#>, <#T##key_length: Int32##Int32#>, <#T##value: UnsafePointer<bson_t>##UnsafePointer<bson_t>#>)
-                
-            case let .Array(array):
-                
-                bson_append_array(<#T##bson: UnsafeMutablePointer<bson_t>##UnsafeMutablePointer<bson_t>#>, <#T##key: UnsafePointer<Int8>##UnsafePointer<Int8>#>, <#T##key_length: Int32##Int32#>, <#T##array: UnsafePointer<bson_t>##UnsafePointer<bson_t>#>)
-            }
+            guard appendValue(documentPointer, key: key, value: value) == true
+                else { return nil }
         }
+        
+        return documentPointer
     }
-    
-    
 }
+
+private extension BSON {
+    
+    static func appendValue(documentPointer: UnsafeMutablePointer<bson_t>, key: String, value: BSON.Value) -> Bool {
+        
+        let keyLength = Int32(key.utf8.count)
+        
+        switch value {
+            
+        case .Null:
+            
+            return bson_append_null(documentPointer, key, keyLength)
+            
+        case let .String(string):
+            
+            let stringLength = Int32(string.utf8.count)
+            
+            return bson_append_utf8(documentPointer, key, keyLength, string, stringLength)
+            
+        case let .Date(date):
+            
+            let secondsSince1970 = Int64(date.timeIntervalSince1970)
+            
+            return bson_append_date_time(documentPointer, key, keyLength, secondsSince1970)
+            
+        case let .Data(data):
+            
+            break
+            
+        case let .Document(childDocument):
+            
+            let childDocumentPointer = UnsafeMutablePointer<bson_t>()
+            
+            bson_append_document_begin(documentPointer, key, keyLength, childDocumentPointer)
+            
+            for (childKey, childValue) in childDocument {
+                
+                appendValue(childDocumentPointer, key: key, value: childValue)
+            }
+            
+            bson_append_document_end(documentPointer, childDocumentPointer)
+            
+        case let .Array(array):
+            
+            let childPointer = UnsafeMutablePointer<bson_t>()
+            
+            bson_append_array_begin(documentPointer, key, keyLength, childPointer)
+            
+            for (index, subvalue) in array.enumerate() {
+                
+                let indexKey = "\(index)"
+                
+                appendValue(childPointer, key: indexKey, value: subvalue)
+            }
+            
+            bson_append_array_end(documentPointer, childPointer)
+        }
+
+    }
+}
+
+
+
+
