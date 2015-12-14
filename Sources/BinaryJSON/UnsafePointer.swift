1c8a39b3d079cf36e2a5a31c031db74aa15ececc
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-13 23:48:09 -0500

    working on appendValue()

--- Sources/BinaryJSON/UnsafePointer.swift
+++ Sources/BinaryJSON/UnsafePointer.swift
@@ -17,7 +17,7 @@ public extension BSON {
     /// Creates an unsafe pointer of a BSON document for use with the C API.
     ///
     /// Make sure to use ```bson_destroy``` clean up the allocated BSON document.
-    static func unsafePointerFromDocument(document: BSON.Document) -> UnsafePointer<bson_t>? {
+    static func unsafePointerFromDocument(document: BSON.Document) -> UnsafeMutablePointer<bson_t>? {
         
         let documentPointer = bson_new()
         
@@ -49,6 +49,15 @@ private extension BSON {
             
             return bson_append_utf8(documentPointer, key, keyLength, string, stringLength)
             
+        case let .Number(number):
+            
+            switch number {
+            case let .Boolean(value): return bson_append_bool(documentPointer, key, keyLength, value)
+            case let .Integer32(value): return bson_append_int32(documentPointer, key, keyLength, value)
+            case let .Integer64(value): return bson_append_int64(documentPointer, key, keyLength, value)
+            case let .Double(value): return bson_append_double(documentPointer, key, keyLength, value)
+            }
+            
         case let .Date(date):
             
             let secondsSince1970 = Int64(date.timeIntervalSince1970)
@@ -57,20 +66,24 @@ private extension BSON {
             
         case let .Data(data):
             
+            // TODO: Implement Binary Data
             break
             
         case let .Document(childDocument):
             
             let childDocumentPointer = UnsafeMutablePointer<bson_t>()
             
-            bson_append_document_begin(documentPointer, key, keyLength, childDocumentPointer)
-            
+            guard bson_append_document_begin(documentPointer, key, keyLength, childDocumentPointer)
+                else { return false }
+        
             for (childKey, childValue) in childDocument {
                 
-                appendValue(childDocumentPointer, key: key, value: childValue)
+                guard appendValue(childDocumentPointer, key: childKey, value: childValue)
+                    else { return false }
             }
             
-            bson_append_document_end(documentPointer, childDocumentPointer)
+            guard bson_append_document_end(documentPointer, childDocumentPointer)
+                else { return false }
             
         case let .Array(array):
             
