ad042265b5ce40f796005ea6d455257cc988d318
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-22 01:36:58 -0500

    implemented toJSONString()

--- Sources/BinaryJSON/JSON.swift
+++ Sources/BinaryJSON/JSON.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2015 PureSwift. All rights reserved.
 //
 
+#if os(OSX)
+    import bson
+#elseif os(Linux)
+    import CBSON
+#endif
+
 import SwiftFoundation
+
+public extension BSON {
+    
+    static func toJSONString(document: BSON.Document) -> String {
+        
+       guard let pointer = unsafePointerFromDocument(document)
+            else { fatalError("Could not convert document to unsafe pointer") }
+        
+        defer { bson_destroy(pointer) }
+        
+        return toJSONString(pointer)
+    }
+    
+    static func toJSONString(unsafePointer: UnsafePointer<bson_t>) -> String {
+        
+        var length = 0
+        
+        let stringBuffer = bson_as_json(unsafePointer, &length)
+        
+        defer { bson_free(stringBuffer) }
+        
+        let string = String.fromCString(stringBuffer)!
+        
+        return string
+    }
+}
--- Sources/UnitTests/BSONTests.swift
+++ Sources/UnitTests/BSONTests.swift
@@ -68,7 +68,7 @@ class BSONTests: XCTestCase {
         // create from pointer
         do {
             
-            print("Document: \n\(document)")
+            print("Document: \n\(document)\n")
             
             guard let unsafePointer = BSON.unsafePointerFromDocument(document)
                 else { XCTFail("Could not create unsafe pointer"); return }
@@ -78,7 +78,7 @@ class BSONTests: XCTestCase {
             guard let newDocument = BSON.documentFromUnsafePointer(unsafePointer)
                 else { XCTFail("Could not create document from unsafe pointer"); return }
             
-            print("New Document: \n\(document)")
+            print("New Document: \n\(document)\n")
             
             XCTAssert(newDocument == document, "\(newDocument) == \(document)")
         }
@@ -86,8 +86,6 @@ class BSONTests: XCTestCase {
         // try to create a 2nd time, to make sure we didnt modify the unsafe pointer
         do {
             
-            print("Document: \n\(document)")
-            
             guard let unsafePointer = BSON.unsafePointerFromDocument(document)
                 else { XCTFail("Could not create unsafe pointer"); return }
             
@@ -96,10 +94,60 @@ class BSONTests: XCTestCase {
             guard let newDocument = BSON.documentFromUnsafePointer(unsafePointer)
                 else { XCTFail("Could not create document from unsafe pointer"); return }
             
-            print("New Document: \n\(document)")
-            
             XCTAssert(newDocument == document, "\(newDocument) == \(document)")
         }
+    }
+    
+    func testToJSON() {
+        
+        var document = BSON.Document()
+        
+        // build BSON document
+        do {
+            
+            let time = TimeInterval(Int(TimeIntervalSince1970()))
+            
+            // Date is more precise than supported by BSON, so equality fails
+            let date = Date(timeIntervalSince1970: time)
+            
+            var numbersDocument = BSON.Document()
+            
+            numbersDocument["double"] = .Number(.Double(1000.1111))
+            
+            numbersDocument["int32"] = .Number(.Integer32(32))
+            
+            numbersDocument["int64"] = .Number(.Integer64(64))
+            
+            document["numbersDocument"] = .Document(numbersDocument)
+            
+            document["string"] = .String("Text")
+            
+            document["array"] = .Array([.Document(["key": .Array([.String("subarray string")])])])
+            
+            document["objectID"] = .ObjectID(BSON.ObjectID())
+            
+            document["datetime"] = .Date(date)
+            
+            document["null"] = .Null
+            
+            document["regex"] = .RegularExpression(BSON.RegularExpression("pattern", options: "\\w"))
+            
+            document["code"] = .Code(BSON.Code("js code"))
+            
+            document["code with scope"] = .Code(BSON.Code("JS code", scope: ["myVariable": .String("value")]))
+            
+            document["timestamp"] = .Timestamp(BSON.Timestamp(time: 10, oridinal: 1))
+            
+            document["minkey"] = .Key(.Minimum)
+            
+            document["maxkey"] = .Key(.Maximum)
+        }
+        
+        print("Document: \n\(document)\n")
+        
+        let jsonString = BSON.toJSONString(document)
+        
+        print("JSON: \n\(jsonString)\n")
     }
     
 }
